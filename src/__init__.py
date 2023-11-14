e05f518878cd4b0e35ce091cb91d2cc3709e2995
LuggiStruggi <lukasmkoenig@gmx.net>
2023-11-14 14:29:51 +0100

    pylint

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
-from .optimizer import DEEPR, softDEEPR
+from .optimizer import DEEPR, SoftDEEPR
 from .converter import convert_to_deep_rewireable, convert_from_deep_rewireable
--- src/utils.py
+++ src/utils.py
@@ -1,41 +1,56 @@
+"""
+provides some useful functions
+"""
+
 import torch
-from torch import nn
-import time    
 
 def measure_sparsity(parameters, threshold=0):
-	if isinstance(parameters, torch.Tensor):
-		parameters = [parameters]
-	total = 0
-	zeros = 0
-	for p in parameters:
-		if threshold:
-			zeros += (p.abs() < threshold).float().sum()
-		else:
-			zeros += (p == 0).float().sum()
-		total += p.numel()
-	return float(zeros/total)
+    """
+    Measures sparsity of a list of tensors or a tensor given a threshold.
+    Sparsity is the ratio of 0s to total elements.
+    """
+    if isinstance(parameters, torch.Tensor):
+        parameters = [parameters]
+    total = 0
+    zeros = 0
+    for p in parameters:
+        if threshold:
+            zeros += (p.abs() < threshold).float().sum()
+        else:
+            zeros += (p == 0).float().sum()
+        total += p.numel()
+    return float(zeros/total)
 
-class progressBar:
+class ProgressBar:
+    """
+    Depicts progress
+    """
 
-	def __init__(self, max_value: int, length: int):
-		self.counter = 0
-		self.length = length
-		self.max_value = max_value
+    def __init__(self, max_value: int, length: int):
+        self.counter = 0
+        self.length = length
+        self.max_value = max_value
 
-	def increment(self, step: int = 1):
-		self.counter += step
-		if self.counter > self.max_value:
-			self.counter = self.max_value
+    def increment(self, step: int = 1):
+        """
+        Increment the counter by step
+        """
+        self.counter += step
+        if self.counter > self.max_value:
+            self.counter = self.max_value
 
-	def prnt(self):
-		return print(f'  Progress: ['+('-'*int(self.counter*self.length/self.max_value)).ljust(self.length, ' ')+']', end = '\r')
+    def prnt(self):
+        """
+        Print the bar to console
+        """
+        print('  Progress: ['+('-'*int(self.counter*self.length/self.max_value)).ljust(self.length,
+                            ' ')+']', end = '\r')
 
-	def reset(self):
-		self.counter = 0
+    def reset(self):
+        """
+        Reset the bar
+        """
+        self.counter = 0
 
 if __name__ == '__main__':
-	a = progressBar(100, 20)
-	while True:
-		a.prnt()
-		a.increment()
-		time.sleep(0.2)
+    pass
